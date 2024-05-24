e7d25f6c913ac615b06c46a01322074ed81c2727
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-05-24 15:05:00 +0000

    Update UefiDiskAccess.c

--- src/UefiDiskAccess.c
+++ src/UefiDiskAccess.c
@@ -60,7 +60,7 @@ INTN EfiCompareGuid(EFI_GUID *Guid1,EFI_GUID *Guid2)
     return 0;
 }
 
-void EnablePageBreak()
+EFI_STATUS EnablePageBreak()
 {
 		EFI_SHELL_PROTOCOL *ShellProtocol;
 		EFI_STATUS STATUS = gBS->LocateProtocol(
@@ -68,6 +68,7 @@ void EnablePageBreak()
 				NULL,
 				(VOID **)&ShellProtocol);
 		ShellProtocol->EnablePageBreak();
+		return EFI_SUCCESS;
 }
 
 void SetConsoleModeToMaximumRows()
