fb9d2c3471d6fab58210105c0c17bc0487c6f074
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-05-25 14:21:55 +0000

    Update UefiDiskAccess.c

--- src/UefiDiskAccess.c
+++ src/UefiDiskAccess.c
@@ -334,7 +334,7 @@ EFI_STATUS EFIAPI UefiDiskAccessMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TA
 		UINT16 RevHi = (UINT16)(SystemTable->Hdr.Revision >> 16);
 		UINT16 RevLo = (UINT16)(SystemTable->Hdr.Revision & 0xFFFF);
 		SetConsoleModeToMaximumRows();
-		EnablePageBreak();
+		//EnablePageBreak();
 		Print(L"UefiDiskAccess Demo - Simple Demo of Accessing Disks in UEFI\r\n");
 		Print(L"Powered by zero.tangptr@gmail.com, Copyright Zero Tang, 2021, All Rights Reserved.\r\n");
 		Print(L"UEFI Firmware Vendor: %s Revision: %d.%d\n", SystemTable->FirmwareVendor, RevHi, RevLo);
