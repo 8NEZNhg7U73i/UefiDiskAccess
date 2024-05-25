302a4e98909a3edd197f05deac4703742d1f0403
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-05-25 07:57:27 +0000

    Update UefiDiskAccess.c

--- src/UefiDiskAccess.c
+++ src/UefiDiskAccess.c
@@ -199,7 +199,10 @@ EFI_STATUS EnumDiskPartitions(IN EFI_BLOCK_IO_PROTOCOL *BlockIoProtocol)
 													{
 														Print(L"Part Type : wre");
 													}
-													Print(L"Part Type GUID:    {%g}\n",&PartitionEntry->PartitionTypeGUID);
+													else
+													{
+														Print(L"Part Type GUID:    {%g}\n",&PartitionEntry->PartitionTypeGUID);
+													}
 													Print(L"Unique Part GUID:  {%g}\n",&PartitionEntry->UniquePartitionGUID);
 												}
 											}
